f359ee052bbdc40f35e349a22a01835cef60a3c3
ujin-Bang <qkddnwls14@gmail.com>
2021-11-05 11:40:08 +0900

    두번째 프래그먼트 제작 (미배치)

--- app/src/main/java/com/neppplus/fragment_20211105/fragments/SecondFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20211105/fragments/SecondFragment.kt
@@ -1,40 +1,35 @@
 package com.neppplus.fragment_20211105.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20211105.R
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
-
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-                btnLog.setOnClickListener {
-
-            Log.d("첫프래그먼트","테스트로그")
-        }
-
         btnToast.setOnClickListener {
-            Toast.makeText(requireContext(), "텍스트", Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(requireContext(), "두번째 프래그먼트", Toast.LENGTH_SHORT).show()
+
+
 
         }
+
     }
-
-
 
 }
